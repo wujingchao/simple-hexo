9882a3bb1adad9a277fb64e7e94a7dcb9adaddc8
wujingchao <wujingchao92@gmail.com>
2016-01-09 10:19:28 +0800

    add flow chart

---
+++ source/js/common.js
@@ -0,0 +1,13 @@
+$(document).ready(function() { 
+	  $(".diagram_sequence").sequenceDiagram({theme: 'simple'});
+      var size = $(".diagram_flow").size();
+      for (var i = 0; i < size; i ++) {
+          var node = $(".diagram_flow").get(i);
+          var textContent = node.textContent;
+          node.textContent = null;
+          node.id = "canvas" + i;
+          $("#" + node.id).css("visibility","visible");
+          var chart = flowchart.parse(textContent);
+          chart.drawSVG(node.id);
+      }
+}); 
